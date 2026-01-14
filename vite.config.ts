f516fcca7361b667480be786561ee631d75c4b9a
fresquitoyconritmo-collab <fresquitoyconritmo@gmail.com>
2026-01-14 19:23:23 +0100

    suegunda subida completa v7

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env': {
+      API_KEY: JSON.stringify(process.env.API_KEY)
+    }
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
-  },
-  server: {
-    port: 3000
+    sourcemap: false
   }
 });
